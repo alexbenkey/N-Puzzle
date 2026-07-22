bacb311231819a6fa0c7c43e328f98a89ed25ec0
OthelloPlusPlus <othello@othel.nl>
2026-07-22 17:33:12 +0200

    [RM] removed deprecated header file

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,10 @@
 /*   By: othello <othello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/10 17:52:53 by ohengelm          #+#    #+#             */
-/*   Updated: 2026/07/22 17:06:15 by othello          ###   ########.fr       */
+/*   Updated: 2026/07/22 17:32:08 by othello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "nPuzzleHeader.hpp"
 #include "nPuzzle.hpp"
 #include "displayNPuzzle.hpp"
 #include <iostream>
