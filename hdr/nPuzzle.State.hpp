2ea2c496f9cf76133c22a01c41c7fe121a9e7f4f
alexbenkey <alex@vonbenckendorff.net>
2026-08-06 20:34:16 +0200

    added the option for search Modes, because bonus

--- hdr/nPuzzle.State.hpp
+++ hdr/nPuzzle.State.hpp
@@ -6,7 +6,7 @@
 /*   By: avon-ben <avon-ben@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 14:32:56 by ohengelm          #+#    #+#             */
-/*   Updated: 2026/07/30 17:24:11 by avon-ben         ###   ########.fr       */
+/*   Updated: 2026/08/06 20:05:53 by avon-ben         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,9 @@ class nPuzzle::State
 	private:
 
 	protected:
+
 		// std::vector<std::vector<Tile> >	tiles;
 		Board	board;
-
 		struct Position {
 			int32_t x;
 			int32_t y;
@@ -45,6 +45,7 @@ class nPuzzle::State
 		int32_t	cost = 0;
 		int32_t score = 0;
 		nPuzzle::State *previous = nullptr;
+		nPuzzle::searchMode searchMode = nPuzzle::searchMode::ASTAR;
 
 		Position emptyPos = {0, 0};
 
@@ -66,6 +67,8 @@ class nPuzzle::State
 		bool	setUsedHeuristic(int32_t value);
 		void	generateTiles(void);
 
+		void setSearchMode(nPuzzle::searchMode mode) {this->searchMode = mode;}
+
 		void	addTile(const int32_t value, const int32_t x, const int32_t y) { this->board.addTile(value, x, y); }
 		// Tile&		getTile(const int32_t value);
 		const nPuzzle::Board::Tile&	getTile(const int32_t value) const;
@@ -75,7 +78,6 @@ class nPuzzle::State
 		// Tile& 		getEmptyTile() { return tiles[emptyPos.y][emptyPos.x]; }
 		const nPuzzle::Board::Tile&	getEmptyTile() const { return this->board.getEmptyTile(); }
 		const nPuzzle::Board&	getBoard() const { return this->board; }
-
 
 		void setPrevious(nPuzzle::State *state) {this->previous = state;}
 		const nPuzzle::State *getPrevious(void) const { return this->previous;}
@@ -100,14 +102,14 @@ class nPuzzle::State
 		int32_t	getCost(void) const	{ return (this->cost); }
 		// int32_t getHeuristic(void) const {return (this->heuristic); }
 
-		bool			sameBoard(const State &rhs) const noexcept;
+		bool	sameBoard(const State &rhs) const noexcept;
 		
 		State	&operator=(const State &src);
 
-		bool			operator<(const State &rhs) const noexcept;
-		bool			operator<=(const State &rhs) const noexcept;
-		bool			operator>(const State &rhs) const noexcept;
-		bool			operator>=(const State &rhs) const noexcept;
+		bool	operator<(const State &rhs) const noexcept;
+		bool	operator<=(const State &rhs) const noexcept;
+		bool	operator>(const State &rhs) const noexcept;
+		bool	operator>=(const State &rhs) const noexcept;
 
 		friend std::ostream&	operator<<(std::ostream& os, const nPuzzle::State& state);
 };
